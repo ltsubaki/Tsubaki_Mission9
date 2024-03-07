4201504fd465b362d50721fc4623f5c9a6d4e88d
ltsubaki <lilian.tsubaki@gmail.com>
2024-03-06 18:50:34 -0700

    Made changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import internal from 'stream';
+import { JSX } from 'react/jsx-runtime';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+const data = require('./CollegeBasketballTeams.json');
+const teamNames = data.teams;
+
+function Welcome() {
+  return <h1>March Madness Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name} </h3>
+        <h3>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {teamNames.map(
+        (
+          oneTeam: JSX.IntrinsicAttributes &
+            JSX.IntrinsicClassAttributes<Team> &
+            Readonly<TeamProps>,
+        ) => (
+          <Team {...oneTeam} />
+        ),
+      )}
+    </div>
+  );
+}
 
 function App() {
-  const blah = 'hi';
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Welcome />
+        <TeamList />
+      </>
     </div>
   );
 }
